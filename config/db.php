3c0dcf7663b0654cde0fc30e0287b185ac78c6ea
vladimirbalin <vladimirzq@Gmail.com>
2022-12-20 01:12:36 +0700

    Rename db in config

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=yii2-simple-api',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=yii2api',
     'username' => 'postgres',
     'password' => '',
     'charset' => 'utf8',
